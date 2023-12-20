75ea0f7350992c444fbf1b4dd949815d5489d12a
Sungjae Park <123699102+spark0510@users.noreply.github.com>
2023-12-20 00:32:25 -0500

    Update install_function.R

--- R/install_function.R
+++ R/install_function.R
@@ -2,6 +2,7 @@ install_function <- function(){
   #readLines("~/.aws/credentials")
   #.libPaths("/tmp")
   #.libPaths()
+  print(.faasr$ComputeServers$My_AWS_Account$Token)
   Sys.getenv("AWS_SESSION_TOKEN")
   Sys.getenv("AWS_ACCESS_KEY_ID")
   Sys.getenv("AWS_DEFAULT_REGION")
