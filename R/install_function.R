a889df4db7e54b315a15c82c0b56a66a2d27e8f2
Sungjae Park <123699102+spark0510@users.noreply.github.com>
2023-12-20 00:40:53 -0500

    Update install_function.R

--- R/install_function.R
+++ R/install_function.R
@@ -2,24 +2,12 @@ install_function <- function(){
   #readLines("~/.aws/credentials")
   #.libPaths("/tmp")
   #.libPaths()
-  Sys.getenv("AWS_SESSION_TOKEN")
-  Sys.getenv("AWS_ACCESS_KEY_ID")
-  Sys.getenv("AWS_DEFAULT_REGION")
-  Sys.unsetenv("AWS_SESSION_TOKEN")
-  Sys.unsetenv("AWS_ACCESS_KEY_ID")
-  Sys.unsetenv("AWS_SECRET_ACCESS_KEY")
-  Sys.setenv(
-          "AWS_ACCESS_KEY_ID" = .faasr$ComputeServers$My_AWS_Account$AccessKey,
-          "AWS_SECRET_ACCESS_KEY" = .faasr$ComputeServers$My_AWS_Account$SecretKey,
-           "AWS_SESSION_TOKEN" = .faasr$ComputeServers$My_AWS_Account$Token,
-            "AWS_DEFAULT_REGION" = "us-east-1")
   #readLines("~/.aws/credentials")
   #print(.faasr$ComputeServers$My_AWS_Account$AccessKey)
-  print(FaaSr::faasr_put_file)
   .libPaths( c( .libPaths(), "/tmp") )
   install.packages("rMR", lib="/tmp")
   library("rMR")
   print("test here///////////////////////")
-  print(FaaSr::faasr_put_file)
+  print(Eq.Ox.conc)
   print("end here/////////////////////////")
   }
