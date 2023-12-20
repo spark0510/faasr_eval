0040359ac5105f29792d29bfc05d96f766335b7a
Sungjae Park <123699102+spark0510@users.noreply.github.com>
2023-12-19 23:56:04 -0500

    Update install_function.R

--- R/install_function.R
+++ R/install_function.R
@@ -3,7 +3,8 @@ install_function <- function(){
   Sys.unsetenv("AWS_ACCESS_KEY_ID")
   Sys.unsetenv("AWS_SECRET_ACCESS_KEY")
   Sys.setenv("AWS_ACCESS_KEY_ID" = .faasr$ComputeServers$My_AWS_Account$AccessKey,
-           "AWS_SECRET_ACCESS_KEY" = .faasr$ComputeServers$My_AWS_Account$SecretKey)
+           "AWS_SECRET_ACCESS_KEY" = .faasr$ComputeServers$My_AWS_Account$SecretKey,
+            "AWS_DEFAULT_REGION" = "us-east-1")
   print(.faasr$ComputeServers$My_AWS_Account$AccessKey)
   .libPaths( c( .libPaths(), "/tmp") )
   install.packages("rMR", lib="/tmp")
