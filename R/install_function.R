7368fbf4f6657ecc6ba448a05ec069513a109cc2
Sungjae Park <123699102+spark0510@users.noreply.github.com>
2023-12-19 23:49:25 -0500

    Update install_function.R

--- R/install_function.R
+++ R/install_function.R
@@ -1,4 +1,6 @@
 install_function <- function(){
+  Sys.unsetenv("AWS_ACCESS_KEY_ID")
+  Sys.unsetenv("AWS_SECRET_ACCESS_KEY")
   Sys.setenv("AWS_ACCESS_KEY_ID" = .faasr$ComputeServers$My_AWS_Account$AccessKey,
            "AWS_SECRET_ACCESS_KEY" = .faasr$ComputeServers$My_AWS_Account$SecretKey)
   .libPaths( c( .libPaths(), "/tmp") )
