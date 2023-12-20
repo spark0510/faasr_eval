b67cb0f9201b022f5bc7af4aca09782a11c9bd4f
Sungjae Park <123699102+spark0510@users.noreply.github.com>
2023-12-19 23:54:55 -0500

    Update install_function.R

--- R/install_function.R
+++ R/install_function.R
@@ -1,4 +1,5 @@
 install_function <- function(){
+  Sys.unsetenv("AWS_SESSION_TOKEN")
   Sys.unsetenv("AWS_ACCESS_KEY_ID")
   Sys.unsetenv("AWS_SECRET_ACCESS_KEY")
   Sys.setenv("AWS_ACCESS_KEY_ID" = .faasr$ComputeServers$My_AWS_Account$AccessKey,
