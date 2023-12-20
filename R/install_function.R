6cccc9be88d7d48d2bc30cf0d32f484c32d0ef74
Sungjae Park <123699102+spark0510@users.noreply.github.com>
2023-12-20 11:51:25 -0500

    Update install_function.R

--- R/install_function.R
+++ R/install_function.R
@@ -1,9 +1,9 @@
 install_function <- function(){
   
-  #.libPaths( c( .libPaths(), "/tmp") )
-  #install.packages("rMR", lib="/tmp")
-  #withr::with_libpaths(new="/tmp", devtools::install_github("eco4cast/neon4cast", force=TRUE))
-  #library("neon4cast")
+  .libPaths( c( .libPaths(), "/tmp") )
+  install.packages("rMR", lib="/tmp")
+  withr::with_libpaths(new="/tmp", devtools::install_github("eco4cast/neon4cast", force=TRUE))
+  library("neon4cast")
   #print("test here///////////////////////")
   #exists("noaa_stage3")
   #print(noaa_stage3)
